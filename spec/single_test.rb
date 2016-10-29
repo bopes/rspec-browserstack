7d1e69a4450437c5ec7b9737bd385541b9422314
bopes <clcblee@gmail.com>
2016-10-28 19:57:31 -0700

    Reference reddit test

--- spec/single_test.rb
+++ spec/single_test.rb
@@ -1,12 +1,15 @@
 require_relative '../scripts/browserstack.rb'
 
-describe "Google's Search Functionality" do
-  it "can find search results" do
-    @driver.navigate.to "https://www.google.com/ncr"
-    element = @driver.find_element(:name, 'q')
-    element.send_keys "BrowserStack"
-    element.submit
-    sleep 5
-    expect(@driver.title).to eql("BrowserStack - Google Search")
-  end
-end
+require_relative 'reddit_test.rb'
+
+# describe "Google's Search Functionality" do
+#   it "can find search results" do
+#     @driver.navigate.to "https://www.google.com/ncr"
+#     element = @driver.find_element(:name, 'q')
+#     element.send_keys "BrowserStack"
+#     element.submit
+#     sleep 5
+#     expect(@driver.title).to eql("BrowserStack - Google Search")
+#   end
+# end
+
